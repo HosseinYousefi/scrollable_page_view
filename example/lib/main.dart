cc83e5cfd40d6b1259be15cbc562a0f4afa5cf0f
Hossein Yousefi <hossein@identi.app>
2021-05-03 19:15:55 +0200

    first working version

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,216 +1,39 @@
+import 'dart:math' show Random;
+
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:scrollable_page_view/scrollable_page_view.dart';
 
 void main() => runApp(App());
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Example());
-  }
-}
-
-class Example extends StatefulWidget {
-  @override
-  _ExampleState createState() => _ExampleState();
-}
-
-class _ExampleState extends State<Example> {
-  late final PageController pageController;
-
-  final widgets = <ScrollableExample>[];
-  final controllers = <ScrollController>[];
-
-  @override
-  void initState() {
-    super.initState();
-    pageController = PageController(initialPage: 1000);
-    for (var i = 0; i < 4; ++i) {
-      controllers.add(ScrollController());
-      widgets.add(ScrollableExample(
-        index: i,
-        controller: controllers[i],
-      ));
-    }
-    WidgetsBinding.instance!.addPostFrameCallback((_) {
-      for (var i = 0; i < 4; ++i) {
-        controllers[i].addListener(() {
-          if (controllers[i].offset > 262) {
-            print('going forwards $i');
-            widgets[i] = ScrollableExample(
-              index: i,
-              controller: controllers[i],
-              scrollable: false,
-            );
-            setState(() {});
-          } else if (controllers[i].offset < 0) {
-            print('going backwards $i');
-            widgets[i] = ScrollableExample(
-              index: i,
-              controller: controllers[i],
-              scrollable: false,
-            );
-            // controllers[(i + 3) % 4].jumpTo(262);
-            widgets[(i + 3) % 4] = ScrollableExample(
-              index: (i + 3) % 4,
-              controller: controllers[(i + 3) % 4],
-              scrollable: false,
-              atEnd: true,
-            );
-            setState(() {});
-          }
-          // print(controller.offset);
-          // print(controller.position);
-        });
-      }
-      // pageController.addListener(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    pageController.dispose();
-    for (final controller in controllers) controller.dispose();
-    super.dispose();
-  }
-
-  bool pageSnapping = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return RawGestureDetector(
-      behavior: HitTestBehavior.opaque,
-      gestures: {
-        AllowMultipleGestureRecognizer: GestureRecognizerFactoryWithHandlers<
-            AllowMultipleGestureRecognizer>(
-          () => AllowMultipleGestureRecognizer(), //constructor
-          (AllowMultipleGestureRecognizer instance) {
-            //initializer
-            instance.onUpdate = (details) {
-              final page = pageController.page!.floor() % 4;
-              if (!widgets[page].scrollable) {
-                final delta = details.delta.dx;
-                // print('offset: ${pageController.offset}');
-                // print('delta: $delta');
-                pageController.jumpTo(pageController.offset - delta);
-                if (pageController.page! % 1 < 0.05) {
-                  print('hiii');
-                  widgets[page] = ScrollableExample(
-                    index: page,
-                    controller: controllers[page],
-                    scrollable: true,
-                  );
-                  setState(() {});
-                }
-              }
-            };
-            instance.onDown = (details) {
-              setState(() {
-                pageSnapping = false;
-              });
-            };
-            instance.onEnd = (details) {
-              print('NOW!');
-              for (var i = 0; i < 4; ++i) {
-                widgets[i] = ScrollableExample(
-                  index: i,
-                  controller: controllers[i],
-                  scrollable: true,
-                );
-              }
-              setState(() {
-                pageSnapping = true;
-              });
-            };
-          },
-        )
-      },
-      child: PageView.builder(
-        pageSnapping: pageSnapping,
-        controller: pageController,
-        physics: AlwaysScrollableScrollPhysics(),
-        itemBuilder: (context, index) => widgets[index % 4],
+    return MaterialApp(
+        home: Material(
+      child: ScrollablePageView(
+        itemBuilder: (BuildContext context, int index) {
+          return Row(
+            children: List.generate(
+              10,
+              (index) => Container(
+                width: 100,
+                color: Color.fromRGBO(
+                  Random().nextInt(256),
+                  Random().nextInt(256),
+                  Random().nextInt(256),
+                  1,
+                ),
+                child: Center(
+                  child: Text('$index'),
+                ),
+              ),
+            ),
+          );
+        },
+        itemCount: 4,
       ),
-    );
-  }
-}
-
-class ScrollableExample extends StatefulWidget {
-  final int index;
-  final ScrollController controller;
-  final bool scrollable;
-  final bool atEnd;
-
-  const ScrollableExample({
-    required this.index,
-    required this.controller,
-    this.scrollable = true,
-    this.atEnd = false,
-  });
-
-  @override
-  _ScrollableExampleState createState() => _ScrollableExampleState();
-}
-
-class _ScrollableExampleState extends State<ScrollableExample> {
-  @override
-  void initState() {
-    super.initState();
-    if (widget.atEnd) {
-      WidgetsBinding.instance!.addPostFrameCallback((_) {
-        widget.controller.jumpTo(262);
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      physics: widget.scrollable
-          ? AlwaysScrollableScrollPhysics()
-          : NeverScrollableScrollPhysics(),
-      controller: widget.controller,
-      scrollDirection: Axis.horizontal,
-      children: [
-        Container(
-          width: 100,
-          color: Colors.red,
-          child: Center(child: Text('${widget.index}')),
-        ),
-        Container(
-          width: 100,
-          color: Colors.yellow,
-          child: Center(child: Text('${widget.index}')),
-        ),
-        Container(
-          width: 100,
-          color: Colors.blue,
-          child: Center(child: Text('${widget.index}')),
-        ),
-        Container(
-          width: 100,
-          color: Colors.green,
-          child: Center(child: Text('${widget.index}')),
-        ),
-        Container(
-          width: 100,
-          color: Colors.brown,
-          child: Center(child: Text('${widget.index}')),
-        ),
-        Container(
-          width: 100,
-          color: Colors.pink,
-          child: Center(child: Text('${widget.index}')),
-        ),
-      ],
-    );
-  }
-}
-
-class AllowMultipleGestureRecognizer extends HorizontalDragGestureRecognizer {
-  @override
-  void rejectGesture(int pointer) {
-    acceptGesture(pointer);
+    ));
   }
 }
